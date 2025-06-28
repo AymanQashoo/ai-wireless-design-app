f5cf65521a92866eb563e86bd4567511f0762a4f
AymanQashoo <165571338+AymanQashoo@users.noreply.github.com>
2025-06-28 21:47:26 +0300

    Update link_budget.py

--- calculations/link_budget.py
+++ calculations/link_budget.py
@@ -1,19 +1,18 @@
 import math
 
-def compute_link_budget(tx_power, tx_gain, rx_gain, distance_km, frequency_mhz, path_loss_exponent):
-    path_loss = (20 * math.log10(distance_km) +
-                 20 * math.log10(frequency_mhz) +
-                 (10 * path_loss_exponent) + 32.44)
-    received_power = tx_power + tx_gain + rx_gain - path_loss
-    link_margin = received_power - (-100)
+def compute_link_budget(required_rx_power, tx_gain, rx_gain, distance_km, frequency_mhz):
+    # Calculate path loss (Free Space Path Loss)
+    path_loss = 20 * math.log10(distance_km) + 20 * math.log10(frequency_mhz) + 32.44
+
+    # Calculate required transmitted power to achieve desired received power
+    required_tx_power = required_rx_power + path_loss - tx_gain - rx_gain
+
+    # Conversely, calculate actual received power if transmitter power was given
+    actual_rx_power = required_tx_power + tx_gain + rx_gain - path_loss
 
     return {
-        "Transmitter Power (dBm)": tx_power,
-        "Transmitter Gain (dBi)": tx_gain,
-        "Receiver Gain (dBi)": rx_gain,
-        "Distance (km)": distance_km,
-        "Frequency (MHz)": frequency_mhz,
-        "Path Loss (dB)": path_loss,
-        "Received Power (dBm)": received_power,
-        "Link Margin (dB)": link_margin
+        "Required Received Signal Strength (dBm)": required_rx_power,
+        "Calculated Path Loss (dB)": path_loss,
+        "Required Transmitted Power (dBm)": required_tx_power,
+        "Actual Received Power (dBm)": actual_rx_power
     }
