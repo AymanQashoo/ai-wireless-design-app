47afda3e4306b4bbd06e41fb459222c4af786260
AymanQashoo <165571338+AymanQashoo@users.noreply.github.com>
2025-06-28 21:38:39 +0300

    Update link_budget.py

--- calculations/link_budget.py
+++ calculations/link_budget.py
@@ -1,9 +1,11 @@
 import math
 
-def compute_link_budget(tx_power, tx_gain, rx_gain, distance_km, frequency_mhz):
-    # Free space path loss (dB)
-    path_loss = 20 * math.log10(distance_km) + 20 * math.log10(frequency_mhz) + 32.44
+def compute_link_budget(tx_power, tx_gain, rx_gain, distance_km, frequency_mhz, path_loss_exponent):
+    path_loss = (20 * math.log10(distance_km) +
+                 20 * math.log10(frequency_mhz) +
+                 (10 * path_loss_exponent) + 32.44)
     received_power = tx_power + tx_gain + rx_gain - path_loss
+    link_margin = received_power - (-100)
 
     return {
         "Transmitter Power (dBm)": tx_power,
@@ -11,6 +13,7 @@ def compute_link_budget(tx_power, tx_gain, rx_gain, distance_km, frequency_mhz):
         "Receiver Gain (dBi)": rx_gain,
         "Distance (km)": distance_km,
         "Frequency (MHz)": frequency_mhz,
-        "Calculated Path Loss (dB)": path_loss,
-        "Received Power (dBm)": received_power
+        "Path Loss (dB)": path_loss,
+        "Received Power (dBm)": received_power,
+        "Link Margin (dB)": link_margin
     }
