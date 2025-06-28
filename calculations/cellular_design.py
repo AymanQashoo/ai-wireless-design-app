24814fb08564a5c93151cf5255305f8ef2153f4b
AymanQashoo <165571338+AymanQashoo@users.noreply.github.com>
2025-06-28 21:38:59 +0300

    Update cellular_design.py

--- calculations/cellular_design.py
+++ calculations/cellular_design.py
@@ -1,19 +1,20 @@
 import math
 
-def compute_cellular_parameters(total_area, cell_radius, cluster_size=7, user_density=1000, bandwidth_per_cell=5):
+def compute_cellular_parameters(total_area, cell_radius, cluster_size,
+                                user_density, bandwidth_per_cell,
+                                traffic_per_user, gos):
     cell_area = (3 * math.sqrt(3) / 2) * (cell_radius ** 2)
     num_cells = math.ceil(total_area / cell_area)
     total_users = total_area * user_density
+    channels_per_cell = math.ceil((user_density * cell_area * traffic_per_user) / gos)
+    max_users_per_cell = channels_per_cell // traffic_per_user
     total_bandwidth_required = num_cells * bandwidth_per_cell
 
     return {
-        "Total Area (km²)": total_area,
-        "Cell Radius (km)": cell_radius,
         "Single Cell Area (km²)": cell_area,
-        "Cluster Size (N)": cluster_size,
-        "Number of Cells Required": num_cells,
-        "User Density (users/km²)": user_density,
-        "Estimated Total Users": total_users,
-        "Bandwidth per Cell (MHz)": bandwidth_per_cell,
-        "Total Network Bandwidth (MHz)": total_bandwidth_required
+        "Number of Cells": num_cells,
+        "Channels per Cell": channels_per_cell,
+        "Max Users per Cell": max_users_per_cell,
+        "Total Users": total_users,
+        "Total Bandwidth Required (MHz)": total_bandwidth_required
     }
